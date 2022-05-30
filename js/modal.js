1c60b8e228a2101295fdc2ba4a981e0677f816d0
Danilo <daniloenju@gmail.com>
2022-05-30 01:57:40 +0100

    Removed Unused assets and fixed responsiveness

--- js/modal.js
+++ js/modal.js
@@ -5,34 +5,25 @@ var img01 = document.getElementById("modalTrigger01");
 var img02 = document.getElementById("modalTrigger02");
 var img03 = document.getElementById("modalTrigger03");
 var modalImg = document.getElementById("modalImg");
-var captionText = document.getElementById("caption");
 
-img01.onclick = function()
-{
+img01.onclick = function () {
   modal.style.display = "flex";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 }
 
-img02.onclick = function()
-{
+img02.onclick = function () {
   modal.style.display = "flex";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 }
 
-img03.onclick = function()
-{
+img03.onclick = function () {
   modal.style.display = "flex";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
 }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() 
-{ 
+modal.onclick = function () {
   modal.style.display = "none";
 }
