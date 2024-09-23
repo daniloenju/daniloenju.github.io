f9675cc555a12df89cbcf9e2add3549ed8605773
Danilo <daniloenju@gmail.com>
2024-09-24 00:21:18 +0100

    Updated structure

--- js/modal.js
+++ js/modal.js
@@ -5,6 +5,7 @@ var modalTriggers = document.getElementsByClassName("modaltrigger")
 var modalTriggersAlt = document.getElementsByClassName("modaltriggerAlt")
 var modalTriggersSquare = document.getElementsByClassName("modaltriggerSquare")
 var modalTriggersVideo = document.getElementsByClassName("modaltriggerVideo")
+var modalTriggersLarge = document.getElementsByClassName("modaltriggerLarge")
 var modalImg = document.getElementById("modalImg");
 var modalVideo = document.getElementById("modalVideo");
 
@@ -44,16 +45,28 @@ for(var index = 0; index < modalTriggersSquare.length; index++)
 }
 
 for(var index = 0; index < modalTriggersVideo.length; index++)
-{
-  modalTriggersVideo[index].onclick = function () 
   {
-    modal.style.display = "flex";
-    modalVideo.src = this.firstElementChild.src;
-    modalImg.style.display = "none"
-    modalVideo.style.display = "block"
-    mybutton.classList.remove("active");
+    modalTriggersVideo[index].onclick = function () 
+    {
+      modal.style.display = "flex";
+      modalVideo.src = this.firstElementChild.src;
+      modalImg.style.display = "none"
+      modalVideo.style.display = "block"
+      mybutton.classList.remove("active");
+    }
   }
-}
+
+for(var index = 0; index < modalTriggersLarge.length; index++)
+  {
+    modalTriggersLarge[index].onclick = function () 
+   {
+    modal.style.display = "flex";
+    modalImg.src = this.src;
+    modalImg.style.display = "block"
+    modalVideo.style.display = "none"
+    mybutton.classList.remove("active");
+    }
+  }
 
 modal.onclick = function () {
   modal.style.display = "none";
